5652bca3ec9431e5c5a889672fefb8437b03f01d
zxs <2668900833@qq.com>
2023-08-08 20:32:27 +0800

    SpringCloud学习资料

--- order-openFeign-sentinel/src/main/java/com/example/order/OrderOpenFeignSentinelApplication.java
+++ order-openFeign-sentinel/src/main/java/com/example/order/OrderOpenFeignSentinelApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//4、加入@EnableFeignClients注解
-
 @SpringBootApplication
 @EnableFeignClients
 public class OrderOpenFeignSentinelApplication {
